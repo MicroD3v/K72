a420ed23ab655d119aff591c6fbe6aa5220813fe
micro0106-boop <mehedihridoy0106@gmail.com>
2026-03-29 02:31:17 +0600

    fix navigation import case

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Agence from "./pages/Agence";
 import Projects from "./pages/Projects";
-import Navbar from "./components/Navigation/Navbar";
-import FullScreenNav from "./components/Navigation/FullScreenNav";
+import Navbar from "./components/navigation/Navbar";
+import FullScreenNav from "./components/navigation/FullScreenNav";
 
 const App = () => {
   return (
